356d01ea8beb4ac04a0499d8fbf965f1f37d1b0f
Valentin Gutknecht <v-gutknecht@outlook.com>
2014-08-07 15:47:53 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hykr::Application.config.secret_key_base = '266ee7ff9d649d887cdad373441f6a91af1f05f17e6a1c1f884eefc4da85b10d1425d3d64bf523c3068dbc5fa77ed90f5be3e817120f675a3ad3a37b5c17b5c1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hykr::Application.config.session_store :cookie_store, key: '_hykr_session'
