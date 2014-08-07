7a94f5ed6a7f80c618ccba14d6620d6d13046608
Valentin Gutknecht <v-gutknecht@outlook.com>
2014-08-07 18:37:18 +0200

    added bootstrap and customizations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Hykr::Application.routes.draw do
+  get "contact" => "pages#contact"
   root "pages#home"
   get "about" => "pages#about"
   # The priority is based upon order of creation: first created -> highest priority.
