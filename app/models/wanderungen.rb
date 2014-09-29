4233238570239f4a54083cfdf4b672e6cdc29c1c
Valentin Gutknecht <v-gutknecht@outlook.com>
2014-09-29 11:17:16 +0200

    added validations for user and wanderungens

--- app/models/wanderungen.rb
+++ app/models/wanderungen.rb
@@ -3,6 +3,7 @@ class Wanderungen < ActiveRecord::Base
 	belongs_to :schwierigkeitsgrad
 	belongs_to :region
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	validates :description, presence: true
 
 
 	def self.search(search)
