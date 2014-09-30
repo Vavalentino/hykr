1e25deaf5d9e93e65bf6aa098406bd19d1166f51
Valentin Gutknecht <v-gutknecht@outlook.com>
2014-09-30 11:38:05 +0200

    added default url for image upload

--- app/models/wanderungen.rb
+++ app/models/wanderungen.rb
@@ -2,7 +2,7 @@ class Wanderungen < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :schwierigkeitsgrad
 	belongs_to :region
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "https://dl.dropboxusercontent.com/u/6677368/e0a019_5e538bfeb7efa2e2781164e5120e35bd.jpg_256.jpg"
 	validates :description, presence: true
 
 
